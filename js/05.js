4a6382604fafd1050438c532dd4e6593fe4dee1a
gilvalencia <44871054+gilvalencia@users.noreply.github.com>
2020-12-09 13:19:10 +0100

    Create 05.js

---
+++ js/05.js
@@ -0,0 +1,28 @@
+// pasos para la creación de un request
+
+var btnCargar = document.getElementById('cargar');
+
+function cargarContenido() {
+  // crearlo
+  var xhr = new XMLHttpRequest();
+
+  // abrirlo
+   xhr.open("GET", "texto_prueba.txt", true);
+   
+   // revisar que cambie
+   xhr.onreadystatechange = function() {
+      //console.log(xhr.readyState);
+      
+      if(xhr.readyState == 4 && xhr.status == 200) {
+          console.log("Se cargo correctamente");
+          
+        
+      } 
+   };
+
+   xhr.send(); 
+}
+
+btnCargar.addEventListener('click', cargarContenido );
+
+
